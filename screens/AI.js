e6d79489bf61e3f3343493c5e58e870f58f9d81a
Ajith pious <ajithpiousolive@gmail.com>
2022-11-05 17:40:11 +0000

    profile page setup and calander without error

--- screens/AI.js
+++ screens/AI.js
@@ -2,17 +2,21 @@ import { SafeAreaView, StyleSheet, TouchableOpacity, View } from 'react-native'
 import React, { useContext } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { CredentialsContext } from '../components/CredentialsContext';
-import { Box, Button, Divider, Heading, Text, HStack, List, ScrollView, Switch, useColorMode, useTheme, Fab, Icon } from "native-base";
+import { Box, Button, Divider, Heading, Text, HStack, List, ScrollView, Switch, useColorMode, useTheme, Fab, Icon, Stack, AspectRatio, Image, Center } from "native-base";
 import { MaterialCommunityIcons, Entypo, Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { DatePicker } from 'react-native-week-month-date-picker';
 import { addDays } from 'date-fns';
+import { Calendar } from 'react-native-calendars';
 export default function AI() {
 
   const { storedCredentials, setStoredCredentials } = useContext(CredentialsContext)
   const navigation = useNavigation();
   const { colorMode, toggleColorMode } = useColorMode();
   const [selectedDate, setSelectedDate] = React.useState(new Date());
+  const vacation = { key: 'vacation', color: 'red', selectedDotColor: 'blue' };
+  const massage = { key: 'massage', color: 'blue', selectedDotColor: 'blue' };
+  const workout = { key: 'workout', color: 'green' };
   const handleLogout = () => {
     AsyncStorage.removeItem('loginCredentials')
       .then(() => {
@@ -33,10 +37,10 @@ export default function AI() {
   return (
     <Box bg={colorMode === 'dark' ? 'black' : 'white'} pt={12}>
       <Heading p={3} mx={2}>
-        My App
+        App Name
       </Heading>
       <ScrollView contentContainerStyle={{ width: '100%' }}>
-        <Divider opacity={colorMode == 'dark' ? '0.4' : '1'} />
+        {/* <Divider opacity={colorMode == 'dark' ? '0.4' : '1'} />
         <Fab renderInPortal={false} shadow={2} size="sm" placement="bottom-right" icon={colorMode == 'light' ?
           <Ionicons
             name="moon-sharp"
@@ -94,24 +98,69 @@ export default function AI() {
               </HStack>
             </List.Item>
           ))}
-        </List>
-
-        <SafeAreaView>
-          <DatePicker
-            startDate={new Date()}
-            maxFutureDays={90}
-            markedDates={[new Date(), addDays(new Date(), 2)]}
-            onDateChange={(date) => setSelectedDate(date)}
-            theme={{
-              primaryColor: 'purple',
-            }}
-          >
-            <View>
-              <Text>Timeslots</Text>
-              <Text>{selectedDate.toString()}</Text>
-            </View>
-          </DatePicker>
-        </SafeAreaView>
+        </List> */}
+        <Box alignItems="center">
+      <Box maxW="80" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1" _dark={{
+      borderColor: "coolGray.600",
+      backgroundColor: "gray.700"
+    }} _web={{
+      shadow: 2,
+      borderWidth: 0
+    }} _light={{
+      backgroundColor: "gray.50"
+    }}>
+        <Box>
+          <AspectRatio w="100%" ratio={16 / 9}>
+            <Image source={{
+            uri: "https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg"
+          }} alt="image" />
+          </AspectRatio>
+        </Box>
+        <Stack p="4" space={3}>
+          <Stack space={2}>
+            <Heading size="md" ml="-1">
+              My .....
+            </Heading>
+          </Stack>
+        </Stack>
+      </Box>
+    </Box>
+    
+    <View style={style2.space}>
+    <Box alignItems="center">
+      <Box maxW="80" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1" _dark={{
+      borderColor: "coolGray.600",
+      backgroundColor: "gray.700"
+    }} _web={{
+      shadow: 2,
+      borderWidth: 0
+    }} _light={{
+      backgroundColor: "gray.50"
+    }}>
+        <Box>
+          <AspectRatio w="100%" ratio={16 / 9}>
+            <Image source={{
+            uri: "https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg"
+          }} alt="image" />
+          </AspectRatio>
+        </Box>
+        <Stack p="4" space={3}>
+          <Stack space={2}>
+            <Heading size="md" ml="-1">
+              sdv
+            </Heading>
+          </Stack>
+        </Stack>
+      </Box>
+    </Box>
+    </View>
+        <Calendar
+          markingType={'multi-dot'}
+          markedDates={{
+            '2017-10-25': { dots: [vacation, massage, workout], selected: true, selectedColor: 'red' },
+            '2017-10-26': { dots: [massage, workout], disabled: true }
+          }}
+        />
       </ScrollView>
     </Box>
   )
@@ -122,5 +171,9 @@ const style2 = {
     marginTop: 50,
     paddingLeft: 10,
     paddingRight: 10
+  },
+  space:{
+    marginTop:10
+
   }
 }
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,10 +1,219 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+// import { View, Text } from 'react-native'
+// import {
+//     Button,
+//     Container,
+//     Icon,
+//     ScreenContainer,
+//     Touchable,
+//     withTheme,
+// } from '@draftbit/ui';
+// import React from 'react'
+// export default function Profile() {
+//     return (<View>
+//         <Text > Profile </Text> 
+//         </View>
+//     )
+// }
 
-export default function Profile() {
+import React from 'react';
+// import Images from '../config/Images';
+import {
+  Button,
+  Container,
+  Icon,
+  ScreenContainer,
+  Touchable,
+  withTheme,
+} from '@draftbit/ui';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+const Profile = props => {
+  const { theme } = props;
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
-}
+    <ScreenContainer
+      style={styles.screenContainerJb}
+      scrollable={true}
+      hasSafeArea={false}
+    >
+      <ImageBackground
+        style={styles.imageBackgroundNb}
+        source={require("../config/Images/img.jpg")}
+        resizeMode="cover"
+      />
+      <Container
+        style={styles.containerEA}
+        elevation={0}
+        useThemeGutterPadding={true}
+      >
+        <Image
+          style={StyleSheet.flatten([
+            styles.imageA3,
+            { borderRadius: theme.borderRadius.global },
+          ])}
+          resizeMode="cover"
+          source={require("../config/Images/img.jpg")}
+        />
+        <Text
+          style={StyleSheet.flatten([
+            styles.textPr,
+            theme.typography.headline3,
+          ])}
+        >
+          Ajith Pious
+        </Text>
+        <Button style={styles.buttonP2} type="outline">
+        <Text color="black"
+        >
+          EDoy
+        </Text>
+        </Button>
+      </Container>
+      <Container useThemeGutterPadding={true} elevation={0}>
+        <Touchable
+          style={StyleSheet.flatten([
+            styles.touchableOk,
+            { borderColor: theme.colors.divider },
+          ])}
+        >
+          <View style={styles.viewKs}>
+            <Text style={theme.typography.body1}>Privacy Settings</Text>
+            <Icon
+              style={styles.iconFE}
+              size={24}
+              color={theme.colors.strong}
+              name="MaterialIcons/account-circle"
+            />
+          </View>
+        </Touchable>
+        <Touchable
+          style={StyleSheet.flatten([
+            styles.touchableOm,
+            { borderColor: theme.colors.divider },
+          ])}
+        >
+          <View style={styles.viewYR}>
+            <Text style={theme.typography.body1}>Notifications</Text>
+            <Icon
+              style={styles.iconCl}
+              color={theme.colors.strong}
+              name="MaterialIcons/notifications"
+              size={24}
+            />
+          </View>
+        </Touchable>
+        <Touchable
+          style={StyleSheet.flatten([
+            styles.touchableBp,
+            { borderColor: theme.colors.divider },
+          ])}
+        >
+          <View style={styles.viewS1}>
+            <Text style={theme.typography.body1}>Order History</Text>
+            <Icon
+              style={styles.iconZz}
+              color={theme.colors.strong}
+              size={24}
+              name="MaterialIcons/history"
+            />
+          </View>
+        </Touchable>
+        <Touchable
+          style={StyleSheet.flatten([
+            styles.touchableJg,
+            { borderColor: theme.colors.divider },
+          ])}
+        >
+          <View style={styles.viewAl}>
+            <Text style={theme.typography.body1}>Payment Details</Text>
+            <Icon
+              style={styles.iconZb}
+              size={24}
+              name="MaterialIcons/payment"
+              color={theme.colors.strong}
+            />
+          </View>
+        </Touchable>
+      </Container>
+    </ScreenContainer>
+  );
+};
+const styles = StyleSheet.create({
+  screenContainerJb: {
+    justifyContent: 'space-evenly',
+  },
+  viewKs: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  viewYR: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  viewS1: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  viewAl: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  imageBackgroundNb: {
+    width: '100%',
+    height: 250,
+  },
+  imageA3: {
+    height: 120,
+    width: 120,
+  },
+  containerEA: {
+    alignItems: 'center',
+    marginTop: -65,
+  },
+  textPr: {
+    width: '100%',
+    textAlign: 'center',
+    marginTop: 16,
+  },
+  touchableOk: {
+    borderTopWidth: 1,
+    paddingTop: 12,
+    paddingBottom: 12,
+    marginTop: 32,
+  },
+  iconFE: {
+    height: 24,
+    width: 24,
+  },
+  iconCl: {
+    width: 24,
+    height: 24,
+  },
+  iconZz: {
+    width: 24,
+    height: 24,
+  },
+  iconZb: {
+    height: 24,
+    width: 24,
+  },
+  buttonP2: {
+    marginTop: 16,
+    alignSelf: 'center',
+    width: '50%',
+  },
+  touchableOm: {
+    paddingBottom: 12,
+    paddingTop: 12,
+    borderTopWidth: 1,
+  },
+  touchableBp: {
+    paddingBottom: 12,
+    paddingTop: 12,
+    borderTopWidth: 1,
+  },
+  touchableJg: {
+    paddingBottom: 12,
+    paddingTop: 12,
+    borderTopWidth: 1,
+  },
+});
+export default withTheme(Profile);
